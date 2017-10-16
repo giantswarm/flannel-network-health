907712d5f25601ff14edb33d047d2b169762bf5a
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-10-16 17:02:24 +0200

    some refactor

--- main.go
+++ main.go
@@ -17,15 +17,14 @@ var (
 	source      string = "https://github.com/giantswarm/flannel-network-health"
 )
 
-const ListenOn = ":8081"
+const (
+	ListenOn = ":8081"
+	MaxRetry = 100
+)
 
 func main() {
 	// for architect
-	if os.Args[1] == "version" {
-		println("flannel network health version 0.1")
-		return
-	}
-	if os.Args[1] == "--help" {
+	if os.Args[1] == "version" || os.Args[1] == "--help" {
 		println("flannel network health version 0.1")
 		return
 	}
@@ -44,7 +43,13 @@ func main() {
 
 	var flannelFile string = os.Getenv("NETWORK_ENV_FILE_PATH")
 	// wait for file creation
-	for {
+	for count := 0; ; count++ {
+		// don't wait forever, if file is not created within retry limit, exit with failure
+		if count > MaxRetry {
+			logger.Log(fmt.Print("After 100sec flannel file is not created. Failure"))
+			os.Exit(1)
+		}
+		// check if file exists
 		if _, err := os.Stat(flannelFile); !os.IsNotExist(err) {
 			break
 		}
